98300631ae6515ce67155ab4ed6d06bab3c9b5b8
Saba <balal.saba@gmail.com>
2025-01-16 23:53:57 +0100

    add tech and categories to admin

--- django-website/portfolio/admin.py
+++ django-website/portfolio/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Technology, Category
 
-admin.site.register(Project)
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'github_link', 'demo_link')
+    search_fields = ('title', 'description')
+    filter_horizontal = ('technologies', 'category')
+    prepopulated_fields = {'slug': ('title',)}
